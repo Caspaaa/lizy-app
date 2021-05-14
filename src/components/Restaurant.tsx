aa4bd3b80ba60d72a419229660356333acbf6698
Caspaaa <e.baudelle@gmail.com>
2021-05-14 21:13:42 +0200

    create RestaurantExtra for rating and price

--- src/components/Restaurant.tsx
+++ src/components/Restaurant.tsx
@@ -1,29 +1,10 @@
 import * as React from "react";
-import parse from "html-react-parser";
 import { RestaurantAddress } from "./RestaurantAdress";
-import starFull from "../assets/images/star-full-icon.png";
-import starHalf from "../assets/images/star-half-icon.png";
-import starEmpty from "../assets/images/star-empty-icon.png";
+import { RestaurantExtras } from "./RestaurantExtras";
 
 interface Props {
   place: Restaurant;
 }
-
-const formatRating = (rating: number) => {
-  const starsFull =
-    `<img src="${starFull}" alt="star-icon-full" class="rating__star" />`.repeat(
-      Math.floor(rating)
-    );
-  const starsHalf =
-    `<img src="${starHalf}" alt="star-icon-half" class="rating__star" />`.repeat(
-      (rating % 1) + 0.5
-    );
-  const starsEmpty =
-    `<img src="${starEmpty}" alt="star-icon-empty" class="rating__star" />`.repeat(
-      Math.floor(5 - rating)
-    );
-  return [starsFull, starsHalf, starsEmpty].join("");
-};
 
 export const Restaurant: React.FunctionComponent<Props> = ({ place }) => {
   return (
@@ -34,13 +15,11 @@ export const Restaurant: React.FunctionComponent<Props> = ({ place }) => {
       <p>{place.name}</p>
       <p>{place.cuisine}</p>
       <RestaurantAddress address={place.address} distance={place.distance} />
-      <div className="restaurant__bottom-extras">
-        <div className="restaurant__rating">
-          <div>{parse(formatRating(place.rating))}</div>
-          <div>({place.rating_count})</div>
-        </div>
-        <div>{place.price}</div>
-      </div>
+      <RestaurantExtras
+        price={place.price}
+        rating={place.rating}
+        rating_count={place.rating_count}
+      />
     </div>
   );
 };
---
+++ src/components/RestaurantExtras.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import parse from "html-react-parser";
+import starFull from "../assets/images/star-full-icon.png";
+import starHalf from "../assets/images/star-half-icon.png";
+import starEmpty from "../assets/images/star-empty-icon.png";
+
+const formatRating = (rating: number) => {
+  const starsFull =
+    `<img src="${starFull}" alt="star-icon-full" class="rating__star" />`.repeat(
+      Math.floor(rating)
+    );
+  const starsHalf =
+    `<img src="${starHalf}" alt="star-icon-half" class="rating__star" />`.repeat(
+      (rating % 1) + 0.5
+    );
+  const starsEmpty =
+    `<img src="${starEmpty}" alt="star-icon-empty" class="rating__star" />`.repeat(
+      Math.floor(5 - rating)
+    );
+  return [starsFull, starsHalf, starsEmpty].join("");
+};
+
+interface Props {
+  price: number;
+  rating: number;
+  rating_count: number;
+}
+
+export const RestaurantExtras: React.FunctionComponent<Props> = ({
+  price,
+  rating,
+  rating_count,
+}) => {
+  return (
+    <div className="restaurant__extras">
+      <div className="restaurant__rating">
+        <div>{parse(formatRating(rating))}</div>
+        <div>({rating_count})</div>
+      </div>
+      <div>{price}</div>
+    </div>
+  );
+};
